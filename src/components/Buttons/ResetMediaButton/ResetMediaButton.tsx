83e6fd8325202952ae1637751ed863c7ba5d1c5b
divya21raj <div21devel@gmail.com>
2020-11-24 09:27:34 +0530

    ResetMediaButton Fix

--- src/components/Buttons/ResetMediaButton/ResetMediaButton.tsx
+++ src/components/Buttons/ResetMediaButton/ResetMediaButton.tsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React from 'react';
 
 import { Button, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { useAppState, useDbState } from "../../../state";
-import { MULTI } from '../../../state/media/media';
-import { initialLocalMedia, initialRemoteMedia } from "../../../state/media/mediaReducers";
-import useVideoContext from "../../../hooks/useVideoContext/useVideoContext";
+import { useAppState, useDbState } from '../../../state';
+import { MULTI, SOURCEMAP } from '../../../state/media/media';
+import { initialLocalMedia, initialRemoteMedia } from '../../../state/media/mediaReducers';
+import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 
 export default function ResetMediaButton() {
+  const { dispatchLocalMedia, dispatchRemoteMedia } = useAppState();
+  const { db, setInDb } = useDbState();
+  const { room } = useVideoContext();
 
-    const { dispatchLocalMedia, dispatchRemoteMedia } = useAppState();
-    const { db, setInDb } = useDbState();
-    const { room } = useVideoContext();
+  const initialMedia = { url: '', source: SOURCEMAP.LOCAL, playing: false, timestamp: 0, fileName: '' };
 
-    const handleClick = (e: any) => {
-        dispatchLocalMedia({ name: MULTI, value: {...initialLocalMedia}});
-        dispatchRemoteMedia({ name: MULTI, value: {...initialRemoteMedia} });
-        setInDb(db, room.name, initialRemoteMedia);
-    }
+  const handleClick = (e: any) => {
+    dispatchLocalMedia({ name: MULTI, value: { ...initialMedia } });
+    dispatchRemoteMedia({ name: MULTI, value: { ...initialMedia } });
+    setInDb(db, room.name, initialMedia);
+  };
 
-    return(
-        <IconButton color="secondary" aria-label="add an alarm" onClick={handleClick}>
-            <DeleteIcon />
-        </IconButton>
-    )
+  return (
+    <IconButton color="secondary" aria-label="add an alarm" onClick={handleClick}>
+      <DeleteIcon />
+    </IconButton>
+  );
 }
